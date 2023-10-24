58d9aa1d2126d7d5d096d826f542dd0e44d2f8b4
Florin Oltean <florin.oltean@metoda.com>
2023-10-24 15:16:57 +0200

    Use fetch to avoid later nil problems.

--- lib/orchparty.rb
+++ lib/orchparty.rb
@@ -26,7 +26,7 @@ module Orchparty
   end
 
   def self.ast(filename:, application:, force_variable_definition: nil)
-    Transformations.transform(Orchparty::DSLParser.new(filename).parse, force_variable_definition: force_variable_definition).applications[application]
+    Transformations.transform(Orchparty::DSLParser.new(filename).parse, force_variable_definition: force_variable_definition).applications.fetch(application)
   end
 
   def self.generate(plugin_name, options, plugin_options)
@@ -34,14 +34,14 @@ module Orchparty
   end
 
   def self.install(cluster_name:, application_name:, force_variable_definition:, file_name:, namespace:)
-    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications[application_name]
+    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications.fetch(application_name)
     namespace = namespace || application_name
     app = KubernetesApplication.new(app_config: app_config, namespace: namespace, cluster_name: cluster_name, file_name: file_name)
     app.install
   end
 
   def self.upgrade(cluster_name:, application_name:, force_variable_definition:, file_name:, namespace:)
-    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications[application_name]
+    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications.fetch(application_name)
     namespace = namespace || application_name
     app = KubernetesApplication.new(app_config: app_config, namespace: namespace, cluster_name: cluster_name, file_name: file_name)
     app.upgrade
@@ -49,7 +49,7 @@ module Orchparty
 
   # NOTE: we no longer make a difference between install and upgrade. We always print the same thing in both cases.
   def self.print(cluster_name:, application_name:, force_variable_definition:, file_name:, namespace:, method:, output:)
-    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications[application_name]
+    app_config = Transformations.transform_kubernetes(Orchparty::Kubernetes::DSLParser.new(file_name).parse, force_variable_definition: force_variable_definition).applications.fetch(application_name)
     namespace = namespace || application_name
     app = KubernetesApplication.new(app_config: app_config, namespace: namespace, cluster_name: cluster_name, file_name: file_name)
     File.open(output, 'w') do |file|
